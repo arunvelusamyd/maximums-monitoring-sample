1fe4e31943c24b55f9737c84c8ef5bbf7d6beb7a
arunvelusamyd <arunvelusamyd@gmail.com>
2021-03-01 16:30:07 +0800

    Added spring boot admin server

---
+++ maximums-sb-admin-server/src/main/java/com/flexy/AdminServerApplication.java
@@ -0,0 +1,19 @@
+package com.flexy;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+@SpringBootApplication
+public class AdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdminServerApplication.class, args);
+    }
+
+}
--- sample-service-consumer/src/main/java/com/flexy/gateway/EmployeeGateway.java
+++ sample-service-consumer/src/main/java/com/flexy/gateway/EmployeeGateway.java
@@ -7,8 +7,6 @@ import io.crnk.core.repository.ResourceRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.sleuth.annotation.ContinueSpan;
-import org.springframework.cloud.sleuth.annotation.NewSpan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
